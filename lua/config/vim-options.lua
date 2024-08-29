126dc751f620e9106e34d98f1b610ffad76b1952
sarathrajendran552 <sarath.tg@gmail.com>
2024-08-29 19:29:58 +0000

    Initial commit of nvim config files

---
+++ lua/config/vim-options.lua
@@ -0,0 +1,10 @@
+vim.o.clipboard = "unnamedplus"
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set rnu")
+vim.cmd("set autoindent")
+vim.cmd("set smartindent")
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
