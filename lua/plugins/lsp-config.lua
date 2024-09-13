193244fe61130b593961c7ad7be56557b1330b44
sarathrajendran552 <sarath.tg@gmail.com>
2024-09-14 02:27:58 +0530

    Auto-sync on Saturday 14 September 2024 02:27:58 AM IST

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,28 +1,35 @@
 return {
-  {
-      "williamboman/mason.nvim",
-      config = function()
-          require("mason").setup()
-      end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-          ensure_installed = { "lua_ls", "tsserver", "snyk_ls", "pylsp", "clangd" }
-        })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({})
-      lspconfig.clangd.setup({})
-      vim.keymap.set('n', '<leader>K', vim.lsp.buf.hover, { desc = "Description under cursor" })
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = "Definition" })
-      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, { desc = "Code Actions" })
-    end
-  }
+    {
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "snyk_ls", "pylsp", "clangd" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+			})
+            lspconfig.pylsp.setup({
+                capabilities = capabilities
+            })
+			vim.keymap.set("n", "<leader>K", vim.lsp.buf.hover, { desc = "Description under cursor" })
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Definition" })
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Actions" })
+		end,
+	},
 }
