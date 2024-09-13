fc0c61a3eb187ed6d36d592615b52a18d8f9c720
sarathrajendran552 <sarath.tg@gmail.com>
2024-09-14 00:27:40 +0530

    Auto-sync on Saturday 14 September 2024 12:27:40 AM IST

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,12 +2,19 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+        -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
     config = function()
-      vim.keymap.set('n', '<C-n>', ':Neotree toggle<CR>', { noremap = true, silent = true, desc = ":Neotree Toggle" })
-  end
+        vim.keymap.set('n', '<C-n>', ':Neotree toggle<CR>', { noremap = true, silent = true, desc = ":Neotree Toggle" })
+        require("neo-tree").setup({
+            close_if_last_window = true,
+            filesystem = {
+                hide_dotfiles = false,
+                hide_hidden = false
+            }
+        })
+    end
 }
